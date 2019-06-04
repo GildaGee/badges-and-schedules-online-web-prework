9366a86329a454d24c35b744d2c164a44edec3af
Gilda Snow <gilda.snow@gmail.com>
2019-06-04 07:27:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,12 @@ def badge_maker(name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  names.map do |names|
-  "Hello, name is #{names}."
+def batch_badge_creator(person)
+  person.map do |person|
+  "Hello, name is #{person}."
 end
 
-def assign_rooms(names)
+def assign_rooms(person)
+  person.each_with_index.map do |person, index|
+    
   
